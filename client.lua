e7d0548da325407cba313d4d6fa39e9681dbbd28
Valentin CHENE <valentin.chene.p@gmail.com>
2025-03-17 21:20:11 +0100

    Reduced interval, fixed for loop, delete peds inside draft

--- client.lua
+++ client.lua
@@ -1,9 +1,11 @@
 function IsAnyDraftHorseWalking(vehicle)
     local harnessCount = GetNumDraftVehicleHarnessPed(GetEntityModel(vehicle))
-    for i = 0, harnessCount - 1 do
-        local horse = GetPedInDraftHarness(vehicle, i)
-        if (DoesEntityExist(horse)) and (IsPedWalking(horse)) then
-            return true
+    if (harnessCount > 0) then
+        for i = 0, harnessCount - 1 do
+            local horse = GetPedInDraftHarness(vehicle, i)
+            if (DoesEntityExist(horse)) and (IsPedWalking(horse)) then
+                return true
+            end
         end
     end
 
@@ -12,16 +14,30 @@ end
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(2000)
+        Citizen.Wait(1500)
         
         local vehicles = GetGamePool("CVehicle")
         for _, vehicle in ipairs(vehicles) do
             if (
-                IsDraftVehicle(vehicle)) and
-                (IsVehicleStopped(vehicle)) and
+                IsDraftVehicle(vehicle) and
+                IsVehicleStopped(vehicle) and
                 (not NetworkGetEntityIsNetworked(vehicle) or NetworkHasControlOfEntity(vehicle))
-                and (IsAnyDraftHorseWalking(vehicle)
+                and IsAnyDraftHorseWalking(vehicle)
             ) then
+                local seatCount = GetVehicleModelNumberOfSeats(GetEntityModel(vehicle))
+                if (seatCount > 0) then
+                    for i = 0, seatCount - 1 do
+                        local ped = GetPedInVehicleSeat(vehicle, i)
+                        if (
+                            DoesEntityExist(ped) and
+                            (not NetworkGetEntityIsNetworked(vehicle) or NetworkHasControlOfEntity(vehicle))
+                        ) then
+                            SetEntityAsMissionEntity(ped, true, true)
+                            DeletePed(ped)
+                        end
+                    end
+                end
+
                 SetEntityAsMissionEntity(vehicle, true, true)
                 DeleteVehicle(vehicle)
             end
